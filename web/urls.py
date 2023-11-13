59cbc0744f77b87569d08b9213e22e4ace05dcf6
David Emiliano calzada morales <dcm74276@lasallebajio.edu.mx>
2023-11-13 14:55:13 -0600

    Se agrega la funcionalidad de crear alumno

--- web/urls.py
+++ web/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import index
+from .views import index, crear_alumno
 
 urlpatterns = [
-    path('', index, name="index")
+    path('', index, name="index"),
+    path('crear_alumno', crear_alumno, name="crear_alumno")
 ]
--- web/views.py
+++ web/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
+from .models import Alumno, Materia
 
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    alumnos = Alumno.objects.all()
+    return render(request, 'index.html', {'alumnos': alumnos})
+
+
+def crear_alumno(request):
+    if request.method == 'POST':
+        nombre = request.POST['nombre']
+        apellido = request.POST['apellido']
+        email = request.POST['email']
+        carrera = request.POST['carrera']
+        semestre = request.POST['semestre']
+        alumno = Alumno(nombre=nombre, apellido=apellido, email=email, carrera=carrera, semestre=semestre)
+        alumno.save()
+        return render(request, 'crud-alumno/crear.html', {'mensaje': 'Alumno creado correctamente'})
+    return render(request, 'crud-alumno/crear.html')
