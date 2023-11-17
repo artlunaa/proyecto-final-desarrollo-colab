a91ce76fb6115273a42ac9c2398b02d45d14f136
David Emiliano calzada morales <dcm74276@lasallebajio.edu.mx>
2023-11-16 19:36:15 -0600

    CRUD de alumno terminado

--- web/urls.py
+++ web/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import index, crear_alumno
+from .views import index, crear_alumno, eliminar_alumo
 
 urlpatterns = [
-    path('', index, name="index"),
-    path('crear_alumno', crear_alumno, name="crear_alumno")
+    path('alumnos', index, name="index"),
+    path('alumnos/crear_alumno', crear_alumno, name="crear_alumno"),
+    path('alumnos/eliminar_alumno', eliminar_alumo, name="eliminar_alumno"),
 ]
--- web/views.py
+++ web/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Alumno, Materia
 
 
@@ -10,12 +10,28 @@ def index(request):
 
 def crear_alumno(request):
     if request.method == 'POST':
+        alumno_id = request.POST.get('id', None)
         nombre = request.POST['nombre']
         apellido = request.POST['apellido']
         email = request.POST['email']
         carrera = request.POST['carrera']
         semestre = request.POST['semestre']
-        alumno = Alumno(nombre=nombre, apellido=apellido, email=email, carrera=carrera, semestre=semestre)
-        alumno.save()
-        return render(request, 'crud-alumno/crear.html', {'mensaje': 'Alumno creado correctamente'})
-    return render(request, 'crud-alumno/crear.html')
+
+        if alumno_id:
+            Alumno.objects.filter(id=alumno_id).update(nombre=nombre, apellido=apellido, email=email, carrera=carrera, semestre=semestre)
+            message = 'Alumno actualizado correctamente'
+        else:
+            Alumno.objects.create(nombre=nombre, apellido=apellido, email=email, carrera=carrera, semestre=semestre)
+            message = 'Alumno creado correctamente'
+        return render(request, 'crud-alumno/crear.html', {'message': message})
+
+    alumno = Alumno.objects.get(id=request.GET.get('id', None)) if request.GET.get('id', None) else None
+    return render(request, 'crud-alumno/crear.html', {'alumno': alumno})
+
+
+def eliminar_alumo(request):
+    if request.method == 'POST':
+        id = request.POST['id']
+        alumno = Alumno.objects.get(id=id)
+        alumno.delete()
+    return redirect('index')
