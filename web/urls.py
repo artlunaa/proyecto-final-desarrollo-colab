94cee9b16010fe47e37e9ba5c650b0f523b6b789
David Emiliano calzada morales <dcm74276@lasallebajio.edu.mx>
2023-11-16 19:58:35 -0600

    CRUD de materias hecho, se agrega una template basica del login

--- web/urls.py
+++ web/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
-from .views import index, crear_alumno, eliminar_alumo
+from .views import index_alumno, crear_alumno, eliminar_alumo, index_materia, crear_materia, eliminar_materia, login, panel
 
 urlpatterns = [
-    path('alumnos', index, name="index"),
+    path('', login, name="login"),
+    path('panel', panel, name="panel"),
+
+    path('alumnos', index_alumno, name="alumno_index"),
     path('alumnos/crear_alumno', crear_alumno, name="crear_alumno"),
     path('alumnos/eliminar_alumno', eliminar_alumo, name="eliminar_alumno"),
+
+    path('materias', index_materia, name="materia_index"),
+    path('materias/crear_materia', crear_materia, name="crear_materia"),
+    path('materias/eliminar_materia', eliminar_materia, name="eliminar_materia"),
 ]
--- web/views.py
+++ web/views.py
@@ -2,10 +2,18 @@ from django.shortcuts import render, redirect
 from .models import Alumno, Materia
 
 
+def login(request):
+    return render(request, 'login.html')
+
+
+def panel(request):
+    return render(request, 'panel.html')
+
+
 # Create your views here.
-def index(request):
+def index_alumno(request):
     alumnos = Alumno.objects.all()
-    return render(request, 'index.html', {'alumnos': alumnos})
+    return render(request, 'alumno/tabla.html', {'alumnos': alumnos})
 
 
 def crear_alumno(request):
@@ -18,15 +26,16 @@ def crear_alumno(request):
         semestre = request.POST['semestre']
 
         if alumno_id:
-            Alumno.objects.filter(id=alumno_id).update(nombre=nombre, apellido=apellido, email=email, carrera=carrera, semestre=semestre)
+            Alumno.objects.filter(id=alumno_id).update(nombre=nombre, apellido=apellido, email=email, carrera=carrera,
+                                                       semestre=semestre)
             message = 'Alumno actualizado correctamente'
         else:
             Alumno.objects.create(nombre=nombre, apellido=apellido, email=email, carrera=carrera, semestre=semestre)
             message = 'Alumno creado correctamente'
-        return render(request, 'crud-alumno/crear.html', {'message': message})
+        return render(request, 'alumno/crear-editar.html', {'message': message})
 
     alumno = Alumno.objects.get(id=request.GET.get('id', None)) if request.GET.get('id', None) else None
-    return render(request, 'crud-alumno/crear.html', {'alumno': alumno})
+    return render(request, 'alumno/crear-editar.html', {'alumno': alumno})
 
 
 def eliminar_alumo(request):
@@ -34,4 +43,37 @@ def eliminar_alumo(request):
         id = request.POST['id']
         alumno = Alumno.objects.get(id=id)
         alumno.delete()
-    return redirect('index')
+    return redirect('alumno_index')
+
+
+def index_materia(request):
+    materias = Materia.objects.all()
+    return render(request, 'materias/tabla.html', {'materias': materias})
+
+
+def crear_materia(request):
+    if request.method == 'POST':
+        materia_id = request.POST.get('id', None)
+        nombre = request.POST['nombre']
+        creditos = request.POST['creditos']
+        profesor = request.POST['profesor']
+
+        if materia_id:
+            Materia.objects.filter(id=materia_id).update(nombre=nombre, creditos=creditos, profesor=profesor)
+            message = 'Materia actualizada correctamente'
+        else:
+            Materia.objects.create(nombre=nombre, creditos=creditos, profesor=profesor)
+            message = 'Materia creada correctamente'
+
+        return render(request, 'materias/crear-editar.html', {'message': message})
+
+    materia = Materia.objects.get(id=request.GET.get('id', None)) if request.GET.get('id', None) else None
+    return render(request, 'materias/crear-editar.html', {'materia': materia})
+
+
+def eliminar_materia(request):
+    if request.method == 'POST':
+        id = request.POST['id']
+        materia = Materia.objects.get(id=id)
+        materia.delete()
+    return redirect('materia_index')
